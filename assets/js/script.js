d74a8cc6b4f07a979a32c221d88d25ccce2e2a61
cassie.cattabiani@gmail.com <cassie.cattabiani@gmail.com>
2020-07-04 11:25:00 -0700

    added city name var and appended to container

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,45 +3,51 @@
 // Current weather API: api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={your api key}
 
 //Global Variables
-var searchButton = document.querySelector("#search-button")
+var searchButton = document.querySelector("#search-button");
 var userSearch = document.querySelector("#search-term").value;
 
-//Make a fetch request to current weather API 
+//Make a fetch request to current weather API
 
-var getWeather = function() {
-    var userSearch = document.querySelector("#search-term").value;
-    var apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + userSearch + '&APPID=a2c9a8e2a17021895f105341626feb6f';
-    console.log(userSearch)
+var getWeather = function () {
+  var userSearch = document.querySelector("#search-term").value;
+  var apiUrl =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    userSearch +
+    "&APPID=a2c9a8e2a17021895f105341626feb6f";
 
-    fetch(apiUrl).then(function(response) {
-        return response.json()
-    }).then(function(response) {
+  fetch(apiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+        console.log(response)
+      // Create city name variable and append to container (need to add date)
+      var cityInfo = document.querySelector("#city-info");
+      var cityName = document.createElement("h3");
+      cityName.textContent = (response.name);
+      cityInfo.appendChild(cityName);
 
-            // //City name variable
-            // var cityName = response.name;
-            
-            // //Temperature variable
-            // var temperature = response.main.temp;
-            // console.log(temperature);
-             
-            // //Humidity variable
-            // var humidity = response.main.humidity;
-            // console.log(humidity);
-             
-            // //Wind speed variable
-            // var windSpeed = response.wind.speed;
-            // console.log(windSpeed);
+
+      //Temperature variable
+      var temperature = response.main.temp;
+
+      //Humidity variable
+      var humidity = response.main.humidity;
+
+      //Wind speed variable
+      var windSpeed = response.wind.speed;
+
+
+
+
     });
-}
+};
 
 //Function grabbing user search in input field
-var searchValue = function() {
-    getWeather();
-    var userSearch = document.querySelector("#search-term").value;
-    // console.log(userSearch)
-}
-
+var searchValue = function () {
+  getWeather();
+  var userSearch = document.querySelector("#search-term").value;
+};
 
 // Event Listener - when button is clicked getWeather function runs
-searchButton.addEventListener("click", searchValue)
-
+searchButton.addEventListener("click", searchValue);
