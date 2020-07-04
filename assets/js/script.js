d128e9f27d51df7474052338027aaafa8c66fdcd
cassie.cattabiani@gmail.com <cassie.cattabiani@gmail.com>
2020-07-04 13:30:33 -0700

    completed uv function

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,28 +20,52 @@ var getWeather = function () {
       return response.json();
     })
     .then(function (response) {
-        console.log(response)
+        console.log("getWeather", response)
       // Create city name variable and append to container (need to add date)
       var cityInfo = document.querySelector("#city-info");
       var cityName = document.createElement("h3");
       cityName.textContent = (response.name);
       cityInfo.appendChild(cityName);
 
-
       //Temperature variable
-      var temperature = response.main.temp;
+      var temperature = document.createElement("p");
+      temperature.textContent = "Temperature: " + response.main.temp + " °F"; // need to convert to °F
+      cityInfo.appendChild(temperature);
 
       //Humidity variable
-      var humidity = response.main.humidity;
+      var humidity = document.createElement("p");
+      humidity.textContent = "Humidity: " + response.main.humidity + " %";
+      cityInfo.appendChild(humidity);
 
       //Wind speed variable
-      var windSpeed = response.wind.speed;
+      var windSpeed = document.createElement("p");
+      windSpeed.textContent = "Wind Speed: " + response.wind.speed + " MPH";
+      cityInfo.appendChild(windSpeed);
 
-
-
-
+       //UV variable 
+       uvIndex(response.coord.lat, response.coord.lon)
+       
     });
 };
+
+// uvIndex function
+var uvIndex = function(lat, lon) {
+    var apiUrl = "http://api.openweathermap.org/data/2.5/uvi/forecast?appid=a2c9a8e2a17021895f105341626feb6f&lat=" + lat + "&lon=" + lon;
+    fetch(apiUrl)
+    .then(function(response) {
+        return response.json();
+    }).then(function(response) {
+    // console.log(response[0].value)
+    var cityInfo = document.querySelector("#city-info");
+    var uv = document.createElement("p");
+    uv.textContent = "UV Index: " + response[0].value;
+    cityInfo.appendChild(uv);
+    })
+}
+
+
+// Forecast function
+
 
 //Function grabbing user search in input field
 var searchValue = function () {
@@ -51,3 +75,4 @@ var searchValue = function () {
 
 // Event Listener - when button is clicked getWeather function runs
 searchButton.addEventListener("click", searchValue);
+
