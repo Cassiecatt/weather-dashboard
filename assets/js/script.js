d9122c159448321694aa9b745ac9f815535d065d
cassie.cattabiani@gmail.com <cassie.cattabiani@gmail.com>
2020-07-04 20:22:19 -0700

    added UV function and starte forecast function

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,18 +13,22 @@ var getWeather = function () {
   var apiUrl =
     "http://api.openweathermap.org/data/2.5/weather?q=" +
     userSearch +
-    "&APPID=a2c9a8e2a17021895f105341626feb6f";
+    "&units=imperial&APPID=a2c9a8e2a17021895f105341626feb6f";
 
   fetch(apiUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (response) {
-        console.log("getWeather", response)
-      // Create city name variable and append to container (need to add date)
+      console.log("getWeather", response);
+
+      //Clear old content 
       var cityInfo = document.querySelector("#city-info");
+      cityInfo.textContent = " ";
+
+      // Create city name variable and append to container (need to add date)
       var cityName = document.createElement("h3");
-      cityName.textContent = (response.name);
+      cityName.textContent = response.name + " (" + new Date().toLocaleDateString() + ")";
       cityInfo.appendChild(cityName);
 
       //Temperature variable
@@ -42,30 +46,57 @@ var getWeather = function () {
       windSpeed.textContent = "Wind Speed: " + response.wind.speed + " MPH";
       cityInfo.appendChild(windSpeed);
 
-       //UV variable 
-       uvIndex(response.coord.lat, response.coord.lon)
-       
+      //UV variable
+      uvIndex(response.coord.lat, response.coord.lon);
+
+      forecast();
     });
 };
 
 // uvIndex function
-var uvIndex = function(lat, lon) {
-    var apiUrl = "http://api.openweathermap.org/data/2.5/uvi/forecast?appid=a2c9a8e2a17021895f105341626feb6f&lat=" + lat + "&lon=" + lon;
-    fetch(apiUrl)
-    .then(function(response) {
-        return response.json();
-    }).then(function(response) {
-    // console.log(response[0].value)
-    var cityInfo = document.querySelector("#city-info");
-    var uv = document.createElement("p");
-    uv.textContent = "UV Index: " + response[0].value;
-    cityInfo.appendChild(uv);
+var uvIndex = function (lat, lon) {
+  var apiUrl =
+    "http://api.openweathermap.org/data/2.5/uvi/forecast?appid=a2c9a8e2a17021895f105341626feb6f&lat=" +
+    lat +
+    "&lon=" +
+    lon;
+  fetch(apiUrl)
+    .then(function (response) {
+      return response.json();
     })
-}
-
+    .then(function (response) {
+      // console.log(response[0].value)
+      var cityInfo = document.querySelector("#city-info");
+      var uv = document.createElement("p");
+      uv.textContent = "UV Index: " + response[0].value;
+      cityInfo.appendChild(uv);
+    });
+};
 
 // Forecast function
+var forecast = function() {
+    var userSearch = document.querySelector("#search-term").value;
+    var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + userSearch + "&appid=a2c9a8e2a17021895f105341626feb6f&lat"
+    fetch(apiUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (response) {
+        console.log("forecast", response)
 
+        //Forecast header
+        var forecast = document.querySelector("#forecast");
+        var forecastHeader = document.createElement("h3");
+        forecastHeader.textContent = "5-Day Forecast";
+        forecast.appendChild(forecastHeader);
+
+        //for loop over forecasts
+        for (var i = 0; i < response.list.length; i++) {
+       
+        }
+
+    });
+};
 
 //Function grabbing user search in input field
 var searchValue = function () {
@@ -75,4 +106,3 @@ var searchValue = function () {
 
 // Event Listener - when button is clicked getWeather function runs
 searchButton.addEventListener("click", searchValue);
-
